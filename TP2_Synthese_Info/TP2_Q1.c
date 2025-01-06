e7252da8df0c5ae4fd766f425e3190697bb6611c
Guilboul40 <161699795+Guilboul40@users.noreply.github.com>
2025-01-06 09:12:19 +0100

    Update TP2_Q1.c

--- TP2_Synthese_Info/TP2_Q1.c
+++ TP2_Synthese_Info/TP2_Q1.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+
 #define fd 1
 #define ERROR_MESSAGE "Erreur dans la commande !\n"
+
 int main(int argc, char* argv[]){
     char *host = argv[1];
     char *file = argv[2];
